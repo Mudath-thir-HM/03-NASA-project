28344efcf5b78bd560ed4b21c3a6e179ff4221a6
Mudath-thir-HM <eighthmudathir@gmail.com>
2025-03-11 09:08:18 +0100

    installed dotenv to use environment variables

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import routes from "./routes";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost/3000",
+    origin: "http://localhost:3000",
   })
 );
 app.use(express.json());
