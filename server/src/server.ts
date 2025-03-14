8a073a970ecf4e98b7d41f92c29d7dc721b2aabc
Mudath-thir-HM <eighthmudathir@gmail.com>
2025-03-14 14:18:04 +0530

    refactor: update server configuration and implement planet data loading

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,17 @@
 import http from "http";
 import app from "./app";
+import { loadPlanetsData } from "./models/planets/planets.model";
 
 const PORT = process.env.PORT as unknown as number;
 
 const server = http.createServer(app);
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
