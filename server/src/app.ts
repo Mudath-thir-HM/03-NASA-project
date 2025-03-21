52eb43f010593a90a6bb78f36e3f9339e9a22615
Mudath-thir-HM <eighthmudathir@gmail.com>
2025-03-21 14:15:19 +0100

    added the logic for getting new launches in the launches service

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import routes from "./routes";
 import path from "path";
 import cors from "cors";
--- server/src/service/launches.service.ts
+++ server/src/service/launches.service.ts
@@ -1,7 +1,22 @@
 import { launches } from "../models/launches/launches.model";
+import { Launches } from "../types/Launches-Types";
+
+let latestFlightNumber = 100;
 
 export const launchesService = {
   parseLaunches() {
     return Array.from(launches.values());
+  },
+  addNewLaunch(launch: Launches) {
+    latestFlightNumber++;
+    launches.set(
+      latestFlightNumber,
+      Object.assign(launch, {
+        flightNumber: latestFlightNumber,
+        customers: ["New Marlo", "NASSA"],
+        upcoming: true,
+        success: true,
+      })
+    );
   },
 };
